0d0562f3a956653ae01a1647db1041ec3688119c
Romario <romariocavalcant@gmail.com>
2020-03-12 21:51:43 -0300

    exercício_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'projeto';
+  pessoas = null;
+  lista =[]
+  cadastrar(){
+    if(this.pessoas != null ){
+      this.pessoas = this.pessoas.trim();
+      if(this.pessoas.length > 0){
+        this.lista.push(this.pessoas);
+      }
+    }
+    this.pessoas = null;
+  }
 }
